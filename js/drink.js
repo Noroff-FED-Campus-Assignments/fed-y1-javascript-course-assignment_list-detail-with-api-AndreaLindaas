aff9728dbf71366db3dd1e78d2bfecd92ccec865
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-02-20 20:59:05 +0100

    added js to detailspage

--- js/drink.js
+++ js/drink.js
@@ -1,13 +1,24 @@
 const querystring = document.location.search;
 const params = new URLSearchParams(querystring);
 const id = params.get("id");
+
 console.log(id);
 
 const url = "http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id;
+const drinkName = document.querySelector(".title");
+const imageOfDrink = document.querySelector(".image-drink");
 
 async function drinkDetails() {
   const response = await fetch(url);
   const result = await response.json();
-  console.log(result);
+
+  showCocktail(result.drinks[0]);
 }
+
+function showCocktail(cocktail) {
+  console.log(cocktail);
+  drinkName.innerHTML = cocktail.strDrink;
+  imageOfDrink.src = cocktail.strDrinkThumb;
+}
+
 drinkDetails();
--- js/drinks.js
+++ js/drinks.js
@@ -1,5 +1,6 @@
 const url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic";
 const drinksHtml = document.querySelector(".drinks");
+const numberOfDrinks = document.querySelector(".drinks-number");
 
 async function getCoctails() {
   const response = await fetch(url);
@@ -8,15 +9,19 @@ async function getCoctails() {
 }
 
 function showCoctails(drinks) {
+  let drinksText = `Number of drinks:${drinks.length}`;
+  numberOfDrinks.innerHTML = drinksText;
   drinksHtml.innerHTML = "";
+
   for (let i = 0; i < drinks.length; i++) {
     console.log(drinks[i]);
 
     let drink = `
-    <div>
+    <div class="drink-image">
         <a href="details.html?id=${drinks[i].idDrink}">
         <img src="${drinks[i].strDrinkThumb}" />
         </a>
+        <p>${drinks[i].strDrink}</p>
     </div>`;
 
     drinksHtml.innerHTML += drink;
