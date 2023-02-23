2f0a3e98b9d1388afec839f582307ce0a53dd56e
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-02-23 13:06:12 +0100

    validated address in javascript

--- js/contact.js
+++ js/contact.js
@@ -2,12 +2,15 @@ const form = document.querySelector("#form");
 const errorMessages = document.querySelector(".error-messages");
 const fName = document.querySelector("#fname");
 const mail = document.querySelector("#email");
+const addressHtml = document.querySelector("#address");
+
 form.onsubmit = function (event) {
   event.preventDefault();
 
   errorMessages.innerHTML = "";
   validateName(fName.value);
   validateEmail(mail.value);
+  validateAddress(address.value);
 };
 
 function validateName(name) {
@@ -29,5 +32,19 @@ function validateEmail(email) {
     mail.classList.add("input-error");
   } else {
     mail.classList.remove("input-error");
+  }
+}
+
+function validateAddress(address) {
+  if (!address) {
+    let message = "<li>Address is required</li>";
+    errorMessages.innerHTML += message;
+    addressHtml.classList.add("input-error");
+  } else if (address.length < 25) {
+    let message = "<li>Address is too short (min 25)</li>";
+    errorMessages.innerHTML += message;
+    addressHtml.classList.add("input-error");
+  } else {
+    addressHtml.classList.remove("input-error");
   }
 }
